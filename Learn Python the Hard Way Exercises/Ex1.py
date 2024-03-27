4d4a8142ecf310a398e345297f9c72de2fa9b7db
joa20297 <162602801+joa20297@users.noreply.github.com>
2024-03-27 11:44:40 +0000

    changed faulty comment

---
+++ Learn Python the Hard Way Exercises/Ex1.py
@@ -0,0 +1,10 @@
+#these are wrong on purpose
+
+#print("Hello World!")
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+print('Hallo ich bin Joa.') 
